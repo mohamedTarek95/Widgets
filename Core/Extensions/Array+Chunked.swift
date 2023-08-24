db9716a6c2368c10d96591e6be085aebdc173f45
Mohamed Tarek <m.tarek.abdelhalim@gmail.com>
2023-08-24 20:05:28 +0300

    chore: documentation for core functions

--- Core/Extensions/Array+Chunked.swift
+++ Core/Extensions/Array+Chunked.swift
@@ -8,12 +8,15 @@
 import Foundation
 
 extension Array {
+    /// Splits an array into chunks of a specified size
     func chunked(into size: Int) -> [[Element]] {
         return stride(from: 0, to: count, by: size).map {
             Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
     
+    /// Splits an array into chunks of a specified size
+    /// - Returns: A tuple of a perfectly chunked array of arrays where the inner arrays are all equal in size and a remainder array for ayny remaining items that couldn't fit into a perfect chunk
     func perfectlyChunked(into size: Int) -> (perfectChunked: [[Element]], remainder: [Element]?) {
         let remainderCount = self.count % size
         let perfectArray: Self
@@ -28,7 +31,7 @@ extension Array {
         return (perfectArray.chunked(into: size), remainder)
     }
     
-    func sameDropLast(_ k: Int) -> Self {
+    private func sameDropLast(_ k: Int) -> Self {
         Array(self.dropLast(k))
     }
 }
