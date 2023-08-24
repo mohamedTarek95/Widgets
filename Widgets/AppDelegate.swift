85257a6fb95f99fb480875daf81c47f786eeee79
Mohamed Tarek <m.tarek.abdelhalim@gmail.com>
2023-08-24 19:59:01 +0300

    refactor: app delegate to use windows instead of scenes

---
+++ Widgets/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Widgets
+//
+//  Created by Mohamed Tarek on 22/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    static var keyWindow: UIWindow? {
+        get {
+            return (UIApplication.shared.delegate as? AppDelegate)?.window
+        }
+        set {
+            (UIApplication.shared.delegate as? AppDelegate)?.window = newValue
+        }
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = AppDelegate.keyWindow ?? UIWindow(frame: UIScreen.main.bounds)
+        AppDelegate.keyWindow = window
+        let controller = HomeSceneConfigurator.configure()
+        controller.view.frame = UIScreen.main.bounds
+        controller.view.layoutIfNeeded()
+        window.makeKeyAndVisible()
+        window.swapRootViewController(newController: controller, animated: false, completion: nil)
+        return true
+    }
+}
